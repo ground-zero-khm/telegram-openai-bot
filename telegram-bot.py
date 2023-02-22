2b9d7ec428eb95848e797946a2658deb0927d9ac
benitamrts <49447249+benitamrts@users.noreply.github.com>
2023-02-22 21:18:38 +0100

    bot update

--- telegram-bot.py
+++ telegram-bot.py
@@ -8,6 +8,8 @@
 # ada model > ada:ft-benita-2022-12-16-13-49-49
 # curie model > curie:ft-benita-2022-12-21-15-41-46
 # curie model V2 > curie:ft-benita-2023-01-24-14-33-22
+# curie model V3 > curie:ft-benita-2023-02-17-12-50-31
+# davinci model V4 > curie:ft-benita-2023-02-21-23-50-26
 
 # bot channel chat id @achtungpropaganda
 
@@ -32,17 +34,17 @@ channel_id = '@achtungpropaganda'
 @bot.message_handler(func=lambda message: True)
 def get_codex(message):
     response = openai.Completion.create(
-        engine="curie:ft-benita-2023-01-24-14-33-22",
+        engine="curie:ft-benita-2023-02-21-23-50-26",
         prompt='"""\n{}\n"""'.format(message.text),
-        temperature=1, # controls randomness, 0 = repetitive
-        max_tokens=200,
+        temperature=0.6, # controls randomness, 0 = repetitive
+        max_tokens=80,
         top_p=1,
         frequency_penalty=0.5, # decreases model's likelihood to repeat the same line verbatim
         presence_penalty=0.5, # increases model's likelihood to talk about new topics
-        stop=['"."'])
+        stop=["\n"])
 
-    #bot.send_message(message.chat.id,
-    bot.send_message(channel_id,
+    #bot.send_message(channel_id,
+    bot.send_message(message.chat.id,
     f'```python\n{response["choices"][0]["text"]}\n```',
     parse_mode="Markdown")
 
